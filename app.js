8321b4b88c416f9e5b10103dad5a262816b50a9a
Abro1986 <AndrewBroestl@gmail.com>
2018-03-27 23:50:16 -0600

    should have commited more

---
+++ app.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('blogForm')
+
+form.addEventListener("submit", function(e){
+
+	e.preventDefault()
+	let inputValue = document.getElementById("blogInput").value
+console.log(inputValue + "is");
+let newElement = document.createElement("p", inputValue + "")
+newElement.innerHTML = inputValue
+document.getElementById('container').appendChild(newElement, inputValue);
+});
+
+
+
+let inputValue = document.getElementById("blogInput").value
+console.log(inputValue + "is");
+let newElement = document.createElement("p")
+newElement.innerHTML = inputValue
+//.getElementById("container").appendChild(newElement.innerHTML)
+//container = document.getElementById("container")
