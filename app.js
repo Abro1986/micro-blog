f68dc47066d0a5f32524db0ced71e7a4affd31f7
Abro1986 <AndrewBroestl@gmail.com>
2018-04-01 23:03:35 -0600

    added a feature to clear the box after input

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const form = document.getElementById('blogForm')
+let input = document.getElementById("blogInput")
+let li = document.getElementsByClassName("list-group-item")
+let button = document.createElement('button');
+let buttonText = document.createTextNode("X")
+
+
 
 form.addEventListener("submit", function(e){
 
 	e.preventDefault()
 	let inputValue = document.getElementById("blogInput").value
+	let newElement = document.createElement('li')
 
-let newElement = document.createElement('li')
-newElement.classList.add('list-group-item');
-newElement.innerHTML = inputValue
-document.getElementById('container').appendChild(newElement, inputValue);
+		newElement.classList.add('list-group-item');
+		newElement.innerHTML = inputValue
+			document.getElementById('container').appendChild(newElement, inputValue , button);
+
+
+
+
+});
+
+form.addEventListener("submit", function(e) {
+ 	e.preventDefault()
+  	input.value = " ";
 });
 
 
+form.addEventListener("sumbit", function(e) {
+	button.appendChild(buttonText);
+	document.getElementById('container').appendChild(button)
+})
 
-//let inputValue = document.getElementById("blogInput").value
-//console.log(inputValue + "is");
-//let newElement = document.createElement("p")
-//newElement.innerHTML = inputValue
-//.getElementById("container").appendChild(newElement.innerHTML)
-//container = document.getElementById("container")
+
